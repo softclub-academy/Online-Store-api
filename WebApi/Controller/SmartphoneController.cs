680939525b1bf5870a78bcc015c8441b625b81a8
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2023-12-04 17:51:25 +0500

    DeleteAnotherControllers

--- WebApi/Controller/SmartphoneController.cs
+++ WebApi/Controller/SmartphoneController.cs
@@ -1,72 +1,72 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net;
-using Domain.Dtos.SmartphoneDtos;
-using Domain.Response;
-using Infrastructure.Services.SmartphoneService;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebApi.Controller;
-
-public class SmartphoneController(ISmartphoneService service) : BaseController
-{
-    [HttpGet("get-smartphones"), AllowAnonymous]
-    public async Task<IActionResult> GetSmartphones()
-    {
-        var result = await service.GetSmartphones();
-        return StatusCode(result.StatusCode, result);
-    }
-
-    // [HttpGet("get-smartphone"), AllowAnonymous]
-    // public async Task<IActionResult> GetSmartphoneById([Required]int id)
-    // {
-    //     if (ModelState.IsValid)
-    //     {
-    //         var result = await service.GetSmartphoneById(id);
-    //         return StatusCode(result.StatusCode, result);
-    //     }
-    //
-    //     var response = new Response<GetSmartphoneDto>(HttpStatusCode.BadRequest, ModelStateErrors());
-    //     return StatusCode(response.StatusCode, response);
-    // }
-
-    [HttpPost("add-smartphone")]
-    public async Task<IActionResult> AddSmartphone([FromForm]AddSmartphoneDto addSmartphone)
-    {
-        if (ModelState.IsValid)
-        {
-            var userId = User.Claims.FirstOrDefault(x => x.Type == "sid")!.Value;
-            var result = await service.AddSmartphone(addSmartphone, userId);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-
-    [HttpPut("update-smartphone")]
-    public async Task<IActionResult> UpdateSmartphone(UpdateSmartphoneDto updateSmartphone)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.UpdateSmartphone(updateSmartphone);
-            return StatusCode(result.StatusCode, Response);
-        }
-
-        var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, Response);
-    }
-
-    [HttpDelete("delete-smartphone")]
-    public async Task<IActionResult> DeleteSmartphone([Required]int id)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.DeleteSmartphone(id);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<bool>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-}
+﻿// using System.ComponentModel.DataAnnotations;
+// using System.Net;
+// using Domain.Dtos.SmartphoneDtos;
+// using Domain.Response;
+// using Infrastructure.Services.SmartphoneService;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace WebApi.Controller;
+//
+// public class SmartphoneController(ISmartphoneService service) : BaseController
+// {
+//     [HttpGet("get-smartphones"), AllowAnonymous]
+//     public async Task<IActionResult> GetSmartphones()
+//     {
+//         var result = await service.GetSmartphones();
+//         return StatusCode(result.StatusCode, result);
+//     }
+//
+//     // [HttpGet("get-smartphone"), AllowAnonymous]
+//     // public async Task<IActionResult> GetSmartphoneById([Required]int id)
+//     // {
+//     //     if (ModelState.IsValid)
+//     //     {
+//     //         var result = await service.GetSmartphoneById(id);
+//     //         return StatusCode(result.StatusCode, result);
+//     //     }
+//     //
+//     //     var response = new Response<GetSmartphoneDto>(HttpStatusCode.BadRequest, ModelStateErrors());
+//     //     return StatusCode(response.StatusCode, response);
+//     // }
+//
+//     [HttpPost("add-smartphone")]
+//     public async Task<IActionResult> AddSmartphone([FromForm]AddSmartphoneDto addSmartphone)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var userId = User.Claims.FirstOrDefault(x => x.Type == "sid")!.Value;
+//             var result = await service.AddSmartphone(addSmartphone, userId);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+//
+//     [HttpPut("update-smartphone")]
+//     public async Task<IActionResult> UpdateSmartphone(UpdateSmartphoneDto updateSmartphone)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.UpdateSmartphone(updateSmartphone);
+//             return StatusCode(result.StatusCode, Response);
+//         }
+//
+//         var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, Response);
+//     }
+//
+//     [HttpDelete("delete-smartphone")]
+//     public async Task<IActionResult> DeleteSmartphone([Required]int id)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.DeleteSmartphone(id);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<bool>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+// }
--- WebApi/Controller/TelevisionController.cs
+++ WebApi/Controller/TelevisionController.cs
@@ -1,71 +1,71 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net;
-using Domain.Dtos.TelevisionDtos;
-using Domain.Response;
-using Infrastructure.Services.TelevisionService;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebApi.Controller;
-
-public class TelevisionController(ITelevisionService service) : BaseController
-{
-    [HttpGet("get-televisions"), AllowAnonymous]
-    public async Task<IActionResult> GetTelevisions()
-    {
-        var result = await service.GetTelevisions();
-        return StatusCode(result.StatusCode, result);
-    }
-
-    [HttpGet("get-television-by-id"), AllowAnonymous]
-    public async Task<IActionResult> GetTelevisionById([Required]int id)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.GetTelevisionById(id);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<GetTelevisionDto>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-
-    [HttpPost("add-television")]
-    public async Task<IActionResult> AddTelevision(AddTelevisionDto addTelevision)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.AddTelevision(addTelevision);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-
-    [HttpPut("update-television")]
-    public async Task<IActionResult> UpdateTelevision(UpdateTelevisionDto updateTelevision)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.UpdateTelevision(updateTelevision);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-
-    [HttpDelete("delete-television")]
-    public async Task<IActionResult> DeleteTelevision([Required]int id)
-    {
-        if (ModelState.IsValid)
-        {
-            var result = await service.DeleteTelevision(id);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        var response = new Response<bool>(HttpStatusCode.BadRequest, ModelStateErrors());
-        return StatusCode(response.StatusCode, response);
-    }
-}
+﻿// using System.ComponentModel.DataAnnotations;
+// using System.Net;
+// using Domain.Dtos.TelevisionDtos;
+// using Domain.Response;
+// using Infrastructure.Services.TelevisionService;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace WebApi.Controller;
+//
+// public class TelevisionController(ITelevisionService service) : BaseController
+// {
+//     [HttpGet("get-televisions"), AllowAnonymous]
+//     public async Task<IActionResult> GetTelevisions()
+//     {
+//         var result = await service.GetTelevisions();
+//         return StatusCode(result.StatusCode, result);
+//     }
+//
+//     [HttpGet("get-television-by-id"), AllowAnonymous]
+//     public async Task<IActionResult> GetTelevisionById([Required]int id)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.GetTelevisionById(id);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<GetTelevisionDto>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+//
+//     [HttpPost("add-television")]
+//     public async Task<IActionResult> AddTelevision(AddTelevisionDto addTelevision)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.AddTelevision(addTelevision);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+//
+//     [HttpPut("update-television")]
+//     public async Task<IActionResult> UpdateTelevision(UpdateTelevisionDto updateTelevision)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.UpdateTelevision(updateTelevision);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<int>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+//
+//     [HttpDelete("delete-television")]
+//     public async Task<IActionResult> DeleteTelevision([Required]int id)
+//     {
+//         if (ModelState.IsValid)
+//         {
+//             var result = await service.DeleteTelevision(id);
+//             return StatusCode(result.StatusCode, result);
+//         }
+//
+//         var response = new Response<bool>(HttpStatusCode.BadRequest, ModelStateErrors());
+//         return StatusCode(response.StatusCode, response);
+//     }
+// }
